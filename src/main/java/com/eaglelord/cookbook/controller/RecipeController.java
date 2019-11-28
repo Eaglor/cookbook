8d00b2cb4703df0dfa81aab912dc69838943c2cd
eaglor <danielsaka@hotmail.com>
2019-11-28 19:06:28 +0100

    recipes

--- src/main/java/com/eaglelord/cookbook/controller/RecipeController.java
+++ src/main/java/com/eaglelord/cookbook/controller/RecipeController.java
@@ -20,26 +20,17 @@ public class RecipeController {
     @ResponseBody
     public List<Recipe> fetchAll() {
         List<Recipe> recipes = new ArrayList<>();
-        Ingredient ingredient = new Ingredient("Onion", "Yellow onion");
-        Recipe recipe = new Recipe("MeatBalls", Arrays.asList(ingredient), "Minced meat");
+        Ingredient i1 = new Ingredient("Onion", "Onion grated");
+        Ingredient i2 = new Ingredient("Minced meat", "Meat from land-living creature chopped in small pieces");
+        Ingredient i3 = new Ingredient("Breadcrumbs", "Breadcrumbs or similar");
+        Ingredient i4 = new Ingredient("Egg", "Egg raw and quickly whipped");
+        Ingredient i5 = new Ingredient("Spices & salt", "Spice it up after taste");
+        Recipe recipe = new Recipe("MeatBalls", Arrays.asList(i1, i2, i3, i4 ), "Soak spices after taste with Onion and Breadcrumbs together for 5 min then add the meat " +
+                "and mix then add egg and mix again with your hands or wooden fork carefully");
         recipes.add(recipe);
         return recipes;
     }
 
 
 
-  /*  @RequestMapping("/addRecipe")
-    public void setRecipe(List<Recipe> recipe) {
-        this.recipe = recipe;
-    }
-
-    @RequestMapping
-    public void addIngredient(String ingredient) {
-        this.ingredient = ingredient;
-    }
-
-    @RequestMapping
-    public String getIngredient() {
-        return this.ingredient;
-    }*/
 }
